719a69110bbdda6b445406ae25049441a114b72b
Mr Khono <mrkhono@192.168.1.6>
2026-03-24 07:49:41 +0000

    removed edit

--- actions/hero.actions.ts
+++ actions/hero.actions.ts
@@ -5,7 +5,7 @@ import { headers } from "next/headers"
 import { auth } from "@/lib/auth"
 import { revalidatePath } from "next/cache"
 
-export const runtime = "nodejs"; 
+
 
 export type HeroContent = {
   title: string
--- actions/sign-in-email.action.ts
+++ actions/sign-in-email.action.ts
@@ -4,31 +4,29 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { APIError } from "better-auth/api";
 
-export const runtime = "nodejs"; 
-
 export async function signInEmailAction(formData: FormData) {
-  const email = String(formData.get("email") ?? "").trim();
+
+  const email = String(formData.get("email"));
   if (!email) return { error: "Entrez un email" };
 
-  const password = String(formData.get("password") ?? "").trim();
+  const password = String(formData.get("password"));
   if (!password) return { error: "Entrez un mot de passe" };
 
   try {
     await auth.api.signInEmail({
       headers: await headers(),
-      body: { email, password },
+      body: {
+        email,
+        password,
+      },
     });
 
-    // Optionnel : récupérer session si besoin
-    // const session = await auth.api.getSession({ headers: await headers() });
-
-    return { success: true, error: null };
+     return { success: true, error: null };
   } catch (err) {
     if (err instanceof APIError) {
+      // return { error: err.message };
       return { error: "Email ou mot de passe invalide" };
     }
-
-    console.error("Erreur signin:", err);
-    return { success: false, error: "Erreur du serveur interne" };
+   return { success: false, error: "Erreur du serveur interne" };
   }
 }
